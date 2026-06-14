2064150540fb00ca7419400424500f4a953da1d0
HazyLunar <erikf08@gmail.com>
2026-06-14 16:12:54 -0700

    Updates and fixes.

--- data/scripts/player/init.lua
+++ data/scripts/player/init.lua
@@ -1,8 +1,4 @@
 
 if onServer() then
-    local oldInit = initialize or function() end
-    function initialize()
-        oldInit()
-        Player():addScriptOnce("data/scripts/player/cosmicascendancycodex.lua")
-    end
+    Player():addScriptOnce("data/scripts/player/cosmicascendancycodex.lua")
 end
